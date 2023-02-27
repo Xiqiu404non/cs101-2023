0decea5e490afb72336ea20c3513f2eccd26d30a
Xiqiu <119796181+Xiqiu404non@users.noreply.github.com>
2023-02-27 15:30:30 +0800

    Update main2.c

--- HW1/main2.c
+++ HW1/main2.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
-void runLengthDecoding(char* str)
+void decode(char* str)
 {
     int len = strlen(str);
-    int count;
-    
-    for (int i = 0; i < len; i++) {
-        count = 0;
-        while (str[i] >= '0' && str[i] <= '9') {
-            count = count * 10 + (str[i] - '0');
-            i++;
-        }
-        for (int j = 0; j < count; j++) {
-            printf("%c", str[i]);
+    for (int i = 0; i < len; i++)
+    {
+        if (isdigit(str[i]))
+        {
+            int num = str[i] - '0';
+            for (int j = 0; j < num; j++)
+            {
+                printf("%c", str[i-1]);
+            }
         }
     }
+    printf("\n");
 }
 
 int main()
 {
-    char str[] = "A3B2C4A9";
-    runLengthDecoding(str);
+    char str[] = "A2B3C4d3";
+    decode(str);
     return 0;
 }
