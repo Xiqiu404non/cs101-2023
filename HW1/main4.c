e1ca3d1b9c9fb262733a4c86da8bb7a655e12fe0
Xiqiu <119796181+Xiqiu404non@users.noreply.github.com>
2023-02-27 16:49:07 +0800

    Update main4.c

--- HW1/main4.c
+++ HW1/main4.c
@@ -1,27 +1,67 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-
-int str_to_int(char* str) {
-    int i = 0;
-    int sign = 1;
-    int result = 0;
-    while (isspace(str[i])) {
-        i++;
-    }
-    if (str[i] == '+' || str[i] == '-') {
-        sign = (str[i++] == '-') ? -1 : 1;
-    }
-    while (isdigit(str[i])) {
-        result = result * 10 + (str[i++] - '0');
-    }
-    return sign * result;
-}
 
 int main() {
-    char a[] = "-99abc";
+    char a[] = "-99acbc";
     char b[] = "99acbc";
     char c[] = "ac-99bc";
-    printf("%d\n%d\n%d", str_to_int(a), str_to_int(b), str_to_int(c));
+    int i, j, num;
+
+    for (i = 0; a[i] != '\0'; i++) {
+        if (a[i] >= '0' && a[i] <= '9') {
+            j = i;
+            while (a[j-1] == '-' && j > 0) {
+                j--;
+            }
+            if (j != i) {
+                printf("-");
+            }
+            while (a[i] >= '0' && a[i] <= '9') {
+                printf("%c", a[i]);
+                i++;
+            }
+            
+            printf(" ");
+            printf("\n");
+        }
+    }
+
+    for (i = 0; b[i] != '\0'; i++) {
+        if (b[i] >= '0' && b[i] <= '9') {
+            j = i;
+            while (b[j-1] == '-' && j > 0) {
+                j--;
+            }
+            if (j != i) {
+                printf("-");
+            }
+            while (b[i] >= '0' && b[i] <= '9') {
+                printf("%c", b[i]);
+                i++;
+            }
+            
+            printf(" ");
+            printf("\n");
+        }
+    }
+
+    for (i = 0; c[i] != '\0'; i++) {
+        if (c[i] >= '0' && c[i] <= '9') {
+            j = i;
+            while (c[j-1] == '-' && j > 0) {
+                j--;
+            }
+            if (j != i) {
+                printf("-");
+            }
+            while (c[i] >= '0' && c[i] <= '9') {
+                printf("%c", c[i]);
+                i++;
+            }
+            
+            printf(" ");
+            printf("\n");
+        }
+    }
+
     return 0;
 }
