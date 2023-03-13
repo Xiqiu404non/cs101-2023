a77ad0122e024a985ffa972bd391a088848ee4ca
Xiqiu <119796181+Xiqiu404non@users.noreply.github.com>
2023-03-14 01:07:10 +0800

    Update main5.c

--- HW1/main5.c
+++ HW1/main5.c
@@ -1,35 +1,21 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-int cmp(const void *a, const void *b) {
-    return *(int*)b - *(int*)a;
+void output(char n){
+    if (n == '8')
+     printf("VIII ");
+    else if(n == '1')
+     printf("I ");
+    else if(n == '6')
+     printf("VI ");
+    else
+     printf("%c ",n);
 }
-
-int main() {
-    char a[] = "-9 8 7 6 5 -4 3 -2 1";
-    int i = 4;
-
-    int nums[9];
-    char *p = strtok(a, " ");
-    int index = 0;
-    while (p != NULL) {
-        nums[index++] = atoi(p);
-        p = strtok(NULL, " ");
-    }
-
-    qsort(nums, sizeof(nums)/sizeof(int), sizeof(int), cmp);
-
-    int count = 0;
-    for (int j = 0; j < sizeof(nums)/sizeof(int); j++) {
-        if (nums[j] != 0) {
-            count++;
-            if (count == i) {
-                printf("%d\n", nums[j]);
-                break;
-            }
-        }
-    }
-
-    return 0;
+ 
+int main()
+{
+    char a[] = "81c8g8u168";
+    int len = sizeof(a)-1;
+    for (int i=0;i<len;i++)
+        output(a[i]);
+    
 }
