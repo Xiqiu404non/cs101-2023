53a2fce188819572a7df9946682aef9b22290fd1
Xiqiu <119796181+Xiqiu404non@users.noreply.github.com>
2023-03-14 00:14:44 +0800

    Update main2.c

--- HW2/main2.c
+++ HW2/main2.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
-int main(){
-	FILE  *fp= fopen("main.c", "r+"), *fpw = fopen("main2.txt", "w+");
-	int c, line_ct = 1;
-	
-	fprintf(fpw,"01  ");
-	while ((c = getc(fp)) != EOF)
-	    c == '\n' ? fprintf(fpw,"\n%02d  ", ++line_ct) : fprintf(fpw,"%c", c);
-	fclose(fp);
-	fclose(fpw);
-	return 0;
+int main()
+{
+    FILE *fp_in ,*fp_out;
+    char c;
+    fp_in = fopen("main2.c","r");
+    fp_out = fopen("main2.txt","w");
+    while ((c = fgetc(fp_in)) != EOF) {
+        fputc(c, fp_out);
+    }
+    fclose(fp_in);
+    fclose(fp_out);
+    return 0;
 }
