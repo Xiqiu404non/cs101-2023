725c34aaa83a387222733266453bef2243b1b619
Xiqiu <119796181+Xiqiu404non@users.noreply.github.com>
2023-03-14 01:05:54 +0800

    Update main2.c

--- HW1/main2.c
+++ HW1/main2.c
@@ -1,27 +1,19 @@
 #include <stdio.h>
-#include <string.h>
-#include <ctype.h>
-
-void decode(char* str)
-{
-    int len = strlen(str);
-    for (int i = 0; i < len; i++)
-    {
-        if (isdigit(str[i]))
-        {
-            int num = str[i] - '0';
-            for (int j = 0; j < num; j++)
-            {
-                printf("%c", str[i-1]);
-            }
-        }
-    }
-    printf("\n");
-}
 
 int main()
 {
-    char str[] = "A2B3C4d3";
-    decode(str);
+    char a[] ="A3B2C4A9";
+    int len = sizeof(a)-1;
+    char word;
+    int num;
+    for (int i=0;i<len;i++){
+        if (i%2){
+            num = a[i]-48;
+            for(int j=0;j<num;j++)
+            printf("%c",word);
+        }else{
+            word = a[i];
+        }
+    }
     return 0;
 }
